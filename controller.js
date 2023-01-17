206e1347783c3e9881ae567b686dbc4434568cd9
RonnieOne <lu.kee@hotmail.co.uk>
2023-01-17 13:56:53 +0000

    Q4 comment count added

--- controller.js
+++ controller.js
@@ -1,16 +1,20 @@
 const { devData } = require("./db/data/development-data/index");
 const { readCategories, readReviews } = require("./model");
 
-const getAllCategories = (request, respsonse) => {
+const getAllCategories = (request, respsonse, next) => {
   readCategories()
     .then((categories) => {
       respsonse.status(200).send({ categories });
     })
-    .catch((err) => {
-      console.log(err);
-    });
+    .catch(next);
 };
 
-const getAllReviews = (request, response) => {};
+const getAllReviews = (request, response, next) => {
+  readReviews()
+    .then((reviews) => {
+      response.status(200).send({ reviews });
+    })
+    .catch(next);
+};
 
 module.exports = { getAllCategories, getAllReviews };
--- model.js
+++ model.js
@@ -8,7 +8,16 @@ readCategories = () => {
 };
 
 readReviews = () => {
-  let queryString = ``;
+  let queryString = `SELECT reviews.* ,
+  COUNT(reviews.review_id)
+  AS comment_count 
+  FROM reviews
+  LEFT JOIN comments ON reviews.review_id = comments.review_id
+  GROUP BY reviews.review_id
+  ORDER BY created_at DESC; `;
+  return db.query(queryString).then((results) => {
+    return results.rows;
+  });
 };
 
 module.exports = { readCategories, readReviews };
